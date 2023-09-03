5ea668d4ce6bac5b245051244c45c73a7ec7120a
Cláudio Pereira <claudiomvpereira@gmail.com>
2023-09-03 03:11:57 -0300

    Correções da última atualização

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,29 +1,14 @@
 export default function initMenuMobile() {
   const hamburger = document.querySelector(".menu-hamburger");
   const menu = document.querySelector(".header-menu");
-  let menuOpen = false;
 
   hamburger.addEventListener("click", () => {
-    menuOpen = !menuOpen;
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
   });
 
-  document.body.addEventListener("click", (event) => {
-    if (
-      menuOpen &&
-      !menu.contains(event.target) &&
-      event.target !== hamburger
-    ) {
-      menuOpen = false;
-      hamburger.classList.remove("active");
-      menu.classList.remove("active");
-    }
-  });
-
   document.querySelectorAll(".menu-mobile").forEach((n) =>
     n.addEventListener("click", () => {
-      menuOpen = false;
       hamburger.classList.remove("active");
       menu.classList.remove("active");
     })
