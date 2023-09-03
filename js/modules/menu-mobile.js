c4311c18d81e23ae30cb39fcdd9d85bd4821c3b2
Cláudio Pereira <claudiomvpereira@gmail.com>
2023-09-03 03:13:49 -0300

    Correção menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,14 +1,29 @@
 export default function initMenuMobile() {
   const hamburger = document.querySelector(".menu-hamburger");
   const menu = document.querySelector(".header-menu");
+  let menuOpen = false;
 
   hamburger.addEventListener("click", () => {
+    menuOpen = !menuOpen;
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
   });
 
+  document.body.addEventListener("click", (event) => {
+    if (
+      menuOpen &&
+      !menu.contains(event.target) &&
+      event.target !== hamburger
+    ) {
+      menuOpen = false;
+      hamburger.classList.remove("active");
+      menu.classList.remove("active");
+    }
+  });
+
   document.querySelectorAll(".menu-mobile").forEach((n) =>
     n.addEventListener("click", () => {
+      menuOpen = false;
       hamburger.classList.remove("active");
       menu.classList.remove("active");
     })
